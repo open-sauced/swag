fd35b8686dce96b6139f6c40415967118bb3e779
Brian Douglas <bdougie@users.noreply.github.com>
2020-08-12 20:48:13 -0700

    Sauce the site (#1)
    * update products
    
    * layout changes

--- pages/index.tsx
+++ pages/index.tsx
@@ -15,14 +15,6 @@ const IndexPage: NextPage = () => {
           </Link>
         </li>
         <li>
-          <Link href="/donate-with-elements">
-            <a className="card elements-style-background">
-              <h2 className="bottom">Donate with Elements</h2>
-              <img src="/elements-card-payment.svg" />
-            </a>
-          </Link>
-        </li>
-        <li>
           <Link href="/use-shopping-cart">
             <a className="card cart-style-background">
               <h2 className="bottom">Use Shopping Cart</h2>
