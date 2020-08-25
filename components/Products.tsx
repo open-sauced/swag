51335089a21a6850ecaed46745a542663564dad3
Brian 'bdougie' Douglas <ilikerobot@gmail.com>
2020-08-25 15:48:01 -0700

    class to className

--- components/Products.tsx
+++ components/Products.tsx
@@ -7,23 +7,23 @@ const Products = () => {
   const {addItem, removeItem} = useShoppingCart();
 
   return (
-    <section class="products">
+    <section className="products">
       {products.map(product => (
-        <div class="max-w-sm rounded overflow-hidden shadow-lg" key={product.sku}>
-          <img class="w-full" src={product.image} alt={product.name} />
-          <div class="px-6 py-4">
-            <p class="text-sm text-darkgrey flex items-center">
-              <h2 class="font-bold text-xl mb-2">{product.name}</h2>
-              <p class="text-xl m-2">
+        <div className="max-w-sm rounded overflow-hidden shadow-lg" key={product.sku}>
+          <img className={product.image} alt={product.name} />
+          <div className="px-6 py-4">
+            <p className="xt-sm text-darkgrey flex items-center">
+              <h2 className="nt-bold text-xl mb-2">{product.name}</h2>
+              <p className="text-xl m-2">
                 {formatCurrencyString({
                   value: product.price,
                   currency: product.currency,
                 })}
               </p>
             </p>
-            <p class="text-lightgrey text-base">{product.description}</p>
+            <p className="text-lightgrey text-base">{product.description}</p>
           </div>
-          <div class="mb-2 flex">
+          <div className="mb-2 flex">
             <button
               className="w-1/2 m-2 min-h-full text-xs bg-green hover:bg-darkgreen text-white font-bold py-2 px-4 rounded"
               onClick={() => addItem(product)}>
