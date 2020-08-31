eb774ee1a6b6044120c6adef9bcfd685e2077b88
thinkverse <hallberg.kim@gmail.com>
2020-09-01 00:06:53 +0200

    Fix landing page design

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,24 +5,26 @@ import Layout from '../components/Layout';
 const IndexPage: NextPage = () => {
   return (
     <Layout title="SWAG |  Open Sauced">
-      <ul className="card-list">
-        <li>
-          <Link href="/donate">
-            <a className="card checkout-style-background">
-              <h2 className="bottom">Donate</h2>
-              <img src="/checkout-one-time-payments.svg" />
-            </a>
-          </Link>
-        </li>
-        <li>
-          <Link href="/stickers">
-            <a className="card cart-style-background">
-              <h2 className="bottom">Stickers</h2>
-              <img style={{borderRadius: 10}} src="/pizza.png" />
-            </a>
-          </Link>
-        </li>
-      </ul>
+      <section className="lg:m-20">
+        <ul className="flex flex-col justify-center items-center lg:justify-start lg:items-start sm:flex-row lg:space-x-12">
+          <li>
+            <Link href="/donate">
+              <a className="card checkout-style-background">
+                <h2 className="bottom">Donate</h2>
+                <img src="/checkout-one-time-payments.svg" />
+              </a>
+            </Link>
+          </li>
+          <li>
+            <Link href="/stickers">
+              <a className="card cart-style-background">
+                <h2 className="bottom">Stickers</h2>
+                <img style={{borderRadius: 10}} src="/pizza.png" />
+              </a>
+            </Link>
+          </li>
+        </ul>
+      </section>
     </Layout>
   );
 };
