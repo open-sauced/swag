d9a93a03835a5da5a1b9b117a88214f399651698
Brian Douglas <bdougie@users.noreply.github.com>
2020-09-05 20:13:02 -0700

    adds merch (#21)

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,10 +8,10 @@ const IndexPage: NextPage = () => {
       <section className="lg:m-20">
         <ul className="flex flex-col justify-center items-center lg:justify-start lg:items-start sm:flex-row lg:space-x-12">
           <li>
-            <Link href="/donate">
+            <Link href="https://merch.streamelements.com/bdougieyo">
               <a className="card checkout-style-background">
-                <h2 className="bottom">Donate</h2>
-                <img src="/checkout-one-time-payments.svg" />
+                <h2 className="bottom">Merch</h2>
+                <img style={{borderRadius: 10}} src="/bdougieyo_merch_panel.png" />
               </a>
             </Link>
           </li>
@@ -22,6 +22,14 @@ const IndexPage: NextPage = () => {
                 <img style={{borderRadius: 10}} src="/pizza.png" />
               </a>
             </Link>
+          </li>
+          <li>
+            {/* <Link href="/donate">
+              <a className="card checkout-style-background">
+                <h2 className="bottom">Donate</h2>
+                <img src="/checkout-one-time-payments.svg" />
+              </a>
+            </Link> */}
           </li>
         </ul>
       </section>
